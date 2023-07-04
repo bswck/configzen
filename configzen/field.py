fd298edc5a39291809359dbb48fded1e7461cca5
bswck <bswck.dev@gmail.com>
2023-07-04 21:06:11 +0200

    Fix ConfigField() annotation

--- configzen/field.py
+++ configzen/field.py
@@ -1,16 +1,16 @@
-from typing import Any, cast
+from typing import Any
 
-from pydantic.fields import Field, FieldInfo, Undefined
+from pydantic.fields import Field, Undefined
 
 __all__ = ("ConfigField",)
 
 
 # noinspection PyPep8Naming
-def ConfigField(default: Any = Undefined, **kwargs: Any) -> FieldInfo:  # noqa: N802
+def ConfigField(default: Any = Undefined, **kwargs: Any) -> Any:  # noqa: N802
     # Since configzen involves BaseSettings implicitly,
     # this would be very convenient to have.
     alias = kwargs.get("alias")
     env = kwargs.get("env")
     if alias is not None and env is None:
         kwargs["env"] = alias
-    return cast(FieldInfo, Field(default, **kwargs))
+    return Field(default, **kwargs)
