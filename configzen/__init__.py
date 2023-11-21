3fcaae50c2a18468eaaf2fd6ca427541acaac46d
bswck <bswck.dev@gmail.com>
2023-11-21 19:51:54 +0100

    Bring back

--- configzen/__init__.py
+++ configzen/__init__.py
@@ -5,3 +5,20 @@ Easily create and maintain complex, statically-typed configurations with validat
 
 (C) 2023-present Bartosz Sławecki (bswck)
 """
+
+from . import configuration, errors, formats, module_proxy, route, sources
+from .configuration import *  # noqa: F403
+from .errors import *  # noqa: F403
+from .formats import *  # noqa: F403
+from .module_proxy import *  # noqa: F403
+from .route import *  # noqa: F403
+from .sources import *  # noqa: F403
+
+__all__ = (
+    configuration.__all__
+    + errors.__all__
+    + formats.__all__
+    + module_proxy.__all__
+    + route.__all__
+    + sources.__all__
+)
