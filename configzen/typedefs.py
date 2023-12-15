d4779b3d063e9d4180985789fd59d6754d3647a5
bswck <bswck.dev@gmail.com>
2023-12-16 00:56:59 +0100

    Simplify typedefs

--- configzen/typedefs.py
+++ configzen/typedefs.py
@@ -6,6 +6,5 @@ from typing import TYPE_CHECKING, TypeVar
 if TYPE_CHECKING:
     from configzen.configuration import BaseConfiguration
 
-    Configuration = TypeVar("Configuration", bound=BaseConfiguration)
-else:
-    Configuration = TypeVar("Configuration", bound="BaseConfiguration")
+
+Configuration = TypeVar("Configuration", bound="BaseConfiguration")
