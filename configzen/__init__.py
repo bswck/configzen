96d8496f0675d22e455385b047baa6d45ea24618
bswck <bswck.dev@gmail.com>
2023-12-18 12:22:30 +0100

    Fix route module import

--- configzen/__init__.py
+++ configzen/__init__.py
@@ -6,12 +6,12 @@ An advanced configuration library for Python.
 (C) 2023-present Bartosz Sławecki (bswck)
 """
 
-from . import configuration, errors, formats, module_proxy, route, sources
+from . import configuration, errors, formats, module_proxy, routes, sources
 from .configuration import *  # noqa: F403
 from .errors import *  # noqa: F403
 from .formats import *  # noqa: F403
 from .module_proxy import *  # noqa: F403
-from .route import *  # noqa: F403
+from .routes import *  # noqa: F403
 from .sources import *  # noqa: F403
 
 __all__ = (
@@ -19,6 +19,6 @@ __all__ = (
     + errors.__all__
     + formats.__all__
     + module_proxy.__all__
-    + route.__all__
+    + routes.__all__
     + sources.__all__
 )
